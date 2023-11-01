e295cd820a474de57f1e4ffc448a9ec1eef49e3a
Daniel Xavier <dxavier194@gmail.com>
2023-11-01 16:23:42 -0300

    CRUD no Angular concluído

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,35 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(private service: PensamentoService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelarPensamento() {
+    alert("Cancelado!")
+  }
+
+}
